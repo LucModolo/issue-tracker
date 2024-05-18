4419b170ee6155afd7e520c90ab6ec5259c78526
Luc <luc.modolo@gmail.com>
2024-05-18 13:30:37 +0200

    Build the new Issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,8 +7,7 @@ const NewIssuePage = () => {
   return (
     <div className='max-w-xl space-y-3'>
       <TextField.Root>
-      
-        
+        <TextField.Input placeholder='Title'/>        
       </TextField.Root>
       <TextArea placeholder='Description' />
       <Button>Submit New Issue</Button>
