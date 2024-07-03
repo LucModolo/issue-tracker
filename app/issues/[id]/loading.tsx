e02fbb4bf28ff805aa9c17dae95f010ba9117715
Luc <luc.modolo@gmail.com>
2024-07-03 20:04:16 +0200

    Refactor: Apply the SRP

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,21 +1,19 @@
-import { Skeleton } from '@/app/componets';
-import { Box, Card, Flex } from '@radix-ui/themes';
-
+import { Skeleton } from "@/app/componets";
+import { Box, Card, Flex } from "@radix-ui/themes";
 
 const LoadingIssueDatailPage = () => {
   return (
-    <Box className='max-w-xl'>
-        <Skeleton />
-        <Flex className='space-x-3' my="2">
-          <Skeleton width="5rem"   />
-          <Skeleton width="8rem"   />
-        </Flex>
-        <Card className='prose' mt="4">
-         <Skeleton count={3}/>
-        </Card>
-        
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex className="space-x-3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
     </Box>
-  )
-}
+  );
+};
 
-export default LoadingIssueDatailPage
+export default LoadingIssueDatailPage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,55 +1,35 @@
-import IssueStatusBadge from '@/app/componets/IssueStatusBadge';
-import prisma from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown';
+import prisma from "@/prisma/client";
+import { Box, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
 
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
-
-
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 //import delay from 'delay';
 
-
 interface Probs {
-    params: {id: string }
+  params: { id: string };
 }
 
-const IssueDetailPage = async ({ params}: Probs ) => {
-   
- const issue = await prisma.issue.findUnique({
-    where: { id:parseInt(params.id)}
- });
+const IssueDetailPage = async ({ params }: Probs) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
- if (!issue)
-    notFound();
+  if (!issue) notFound();
 
+  //await delay(2000);
 
-//await delay(2000);
-
-
-    return (
-    <Grid columns={ {initial: "1", md: "2"} } gap= "5">
-        <Box>
-            <Heading>{issue.title}</Heading>
-            <Flex className='space-x-3' my="2">
-            <IssueStatusBadge status={issue.status} />
-            <Text>{issue.createdAt.toDateString()}</Text>
-
-            </Flex>
-            <Card className='prose' mt="4">
-            <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-        </Box>
-        <Box>
-            <Button>
-                <Pencil2Icon />
-                <Link href= {`/issue/${issue.id}/edit`}> Edit Issue</Link>
-            </Button> 
-        </Box>    
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <IssueDetails issue={issue} />
+      </Box>
+      <Box>
+        <EditIssueButton issueId={issue.id} />
+      </Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
